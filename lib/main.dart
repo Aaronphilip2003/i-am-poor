bf3dd2a4146b6fc0eff1c1dd9b56a4e4d17ef8eb
Aaronphilip2003 <aaronphilip2003@gmail.com>
2021-08-20 17:49:16 +0530

    Second App

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text("I am Poor"),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child: Image(image: AssetImage("images/i_am_poor.png")),
+        ),
+      ),
+    ),
+  );
+}
